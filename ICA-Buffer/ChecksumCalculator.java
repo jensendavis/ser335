febedae38e61cf64afe43e3552a399ced9eadacb
jensendavis <jensendavis2004@gmail.com>
2025-04-08 10:05:23 -0700

    Add files via upload

---
+++ ICA-Buffer/ChecksumCalculator.java
@@ -0,0 +1,11 @@
+public class ChecksumCalculator {
+
+    private static native long calculateChecksum(String filename);
+
+    public static void main(String[] args) {
+
+        System.loadLibrary("main");
+        long checksum = calculateChecksum(args[0]);
+        System.out.println("Got checksum from native method: " + checksum);
+    }
+}
